80060335b3611636fc225bf82aa2fe4c1011dbfd
Huy Dang <ddhuy137@yahoo.com>
2023-04-18 14:37:08 -0500

    Python: SimUDuck double check class/instance variable

--- Python/SimUDuck/SimUDuck.py
+++ Python/SimUDuck/SimUDuck.py
@@ -10,3 +10,12 @@ if __name__ == '__main__':
     model.perform_fly()
     model.set_fly_behavior(FlyRocketPowered())
     model.perform_fly()
+
+    model2 = ModelDuck()
+    model2.perform_fly()
+
+    model.perform_fly()
+
+    print(ModelDuck._fly_behavior)
+    print(model._fly_behavior)
+    print(model2._fly_behavior)
